660ec8b9febea224428a75efdaebb8175cfa08d6
Rainmanwy <wang_yang1980@hotmail.com>
2016-03-14 16:06:00 +0800

    add layer doc generator

--- src/ScapyLibrary/keywords/sendrecv.py
+++ src/ScapyLibrary/keywords/sendrecv.py
@@ -11,6 +11,31 @@ class L3SendRecv(_BaseClass):
 
     def send_and_receive_at_layer3(self, packet, return_answer=True,
                                     return_unanswer=False, return_send=False, *args, **kwargs):
+        '''This keyword support send layer 3 packets and receive the answers, it use "sr" function in scapy
+
+        @packet: Layer 3 packets created by scapy
+        @return_answer: boolean, whether return answered packets
+        @return_unanswer: boolean, whether return unanswered packets
+        @return_send: boolean, whether return sended packets which are answered
+        @args: please check doc of "sr" function in scapy to get detail
+        @kwargs: please check doc of "sr" function in scapy to get detail
+        @return: return sended packets/answered packets/unanswered packets,
+
+        By default, only answered packets are return
+
+        | ${answered} | Send And Receive At Layer3 | ${packets} | timeout=${10} |
+        | Log Packets | ${answered[0]} |
+
+        | ${answered} | Send And Receive At Layer3 | ${packets} | timeout=${10} | return_send=${True} |
+        | Log Packets | ${answered[0][0]} |
+        | Log Packets | ${answered[0][1]} |
+
+        | ${answered} | ${unanswered} | Send And Receive At Layer3 | ${packets} | timeout=${10} | return_send=${True} | return_answer=${True} |
+        | Log Packets | ${answered[0][0]} |
+        | Log Packets | ${answered[0][1]} |
+        | Log Packets | ${unanswered[0]} |
+
+        '''
         an, unan = scapylib.sr(packet, *args, **kwargs)
         answered = scapylib.plist.PacketList(name='Answered')
         returnAnswer = True
